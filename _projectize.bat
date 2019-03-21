f24a516186a2ab8962861dbfeadb25f662eb81d0
matteo <matteo.lacki@gmail.com>
2019-03-21 11:49:27 +0100

    Files point to proper locations.

--- _projectize.bat
+++ _projectize.bat
@@ -2,6 +2,6 @@ rem This script runs projectizer as a SendTo Windows application.
 rem
 rem to make it work, open the explorer, highlight projects you want to analyze with
 rem IsoQuant, right click on any of them and select SendTo/_projectize
-python D:\ms_soft\lab_scripts\projectizer2\projectizer2\projectize.py %* -v
+python C:\Symphony\Utils\projectizer2\projectize.py %* -v
 @echo off
 pause
--- _xmls2jsons.bat
+++ _xmls2jsons.bat
@@ -2,6 +2,6 @@ rem This script parses xmls files and produces a params.json file in the selecte
 rem
 rem to make it work, open the explorer, highlight projects you want to analyze with
 rem IsoQuant, right click on any of them and select SendTo/_projectize
-python D:\ms_soft\lab_scripts\projectizer2\projectizer2\xmls2jsons.py %* -v
+python C:\Symphony\Utils\projectizer2\xmls2jsons.py %* -v
 @echo off
 pause

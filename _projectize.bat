eedee43d728aa7d309b9ea20e9fb76bd69042383
MatteoLacki <matteo.lacki@gmail.com>
2020-02-07 13:54:55 +0100

    updated default path

--- _projectize.bat
+++ _projectize.bat
@@ -2,6 +2,6 @@ rem This script runs projectizer as a SendTo Windows application.
 rem
 rem to make it work, open the explorer, highlight projects you want to analyze with
 rem IsoQuant, right click on any of them and select SendTo/_projectize
-python C:\Symphony\Utils\projectizer2\projectize.py %* -v
+python C:\SYMPHONY_VODKAS\projectizer2\projectize.py %* -v
 @echo off
 pause
